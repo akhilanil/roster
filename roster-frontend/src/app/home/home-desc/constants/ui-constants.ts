5a49d6f855bff2319227bf0c75eb5bc314af9394
akhilanil <akhilanil95@gmail.com>
2018-08-07 00:30:19 +0530

    Home Design

---
+++ roster-frontend/src/app/home/home-desc/constants/ui-constants.ts
@@ -0,0 +1,15 @@
+
+/* Login button */
+export const LOGIN_BUTTON: string = 'Login';
+
+/* Register button */
+export const REGISTER_BUTTON: string = 'Signup';
+
+/* Register button */
+export const CONTINUE_BUTTON: string = 'Continue without login';
+
+/* Heading for home component */
+export const HOME_DESC_HEADING: string = 'Make Rosters on the go..!!';
+
+/* Text for home component */
+export const HOME_DESC_TEXT: string = 'Lorem ipsum dolor sit amet, ne mel stet duis, perfecto dignissim disputando id vim. Illum indoctum ne his, mel tempor voluptaria in. Quo in elaboraret reprimique, vel in zril aliquip epicuri. Ex sumo quot noster nam, ius urbanitas constituto eu, populo iriure eu eum. Fabellas scriptorem deterruisset ex vim.';
--- roster-frontend/src/app/home/home-desc/home-desc.component.ts
+++ roster-frontend/src/app/home/home-desc/home-desc.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+/* UI constants  */
+import { LOGIN_BUTTON } from './constants/ui-constants'
+import { REGISTER_BUTTON } from './constants/ui-constants'
+import { CONTINUE_BUTTON } from './constants/ui-constants'
+import { HOME_DESC_HEADING } from './constants/ui-constants'
+import { HOME_DESC_TEXT } from './constants/ui-constants'
+
+/* Routing URL constants */
+import { LOGIN_URL } from './constants/url-constants'
+import { REGISTER_URL } from './constants/url-constants'
+
+
 @Component({
   selector: 'app-home-desc',
   templateUrl: './home-desc.component.html',
@@ -7,9 +21,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeDescComponent implements OnInit {
 
-  constructor() { }
+
+
+  /* Variable to indicate the state of home view. Either Login or Register */
+  isLogin : boolean;
+
+  /* String representing Login/Signup button name */
+  buttonName : string;
+
+  /* String representing header for home component */
+  homeDescHeader : string;
+
+  /* String representing header for home component */
+  homeDescText : string;
+
+  /* String representing Login/Signup button name */
+  continueButton : string;
+
+  /* Holds the breakpoint constant of the screen. If the screen width is
+     less than 400 then components are aligned vertically making the screen
+      responsive
+   */
+  breakpoint : number;
+
+
+  isGutterRequired : boolean;
+
+
+  constructor(private router: Router) {
+
+    /* Checks the current url and set appropriate value for the route */
+    if(this.router.url === LOGIN_URL){
+      this.isLogin = true
+      this.buttonName = REGISTER_BUTTON
+    } else if(this.router.url === REGISTER_URL){
+      this.isLogin = false
+      this.buttonName = LOGIN_BUTTON
+    }
+
+    this.homeDescHeader = HOME_DESC_HEADING
+    this.homeDescText = HOME_DESC_TEXT
+    this.continueButton = CONTINUE_BUTTON
+  }
 
   ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 400) ? 1 : 2;
+    this.isGutterRequired = (window.innerWidth <= 400) ? false : true;
+  }
+
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 2;
+    this.isGutterRequired = (window.innerWidth <= 400) ? false : true;
+  }
+
+  /*
+    This method handles the routing between login and register when
+    Login/Signup button is clicked
+  */
+  onStateChanged() {
+
+    this.isLogin = !this.isLogin
+    if(this.isLogin) {
+      this.buttonName = REGISTER_BUTTON
+      this.router.navigate([LOGIN_URL])
+
+    } else {
+      this.buttonName = LOGIN_BUTTON
+      this.router.navigate([REGISTER_URL])
+    }
   }
 
 }
